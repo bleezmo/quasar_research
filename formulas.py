b2616e90c032789642b34875e53bdbcf95f11b87
bleezmo <joshuarogers10@gmail.com>
2014-05-02 17:17:42 -0400

    more formula. wip

--- formulas.py
+++ formulas.py
@@ -1,6 +1,15 @@
+from math import *
+
 #gravitational constant
 G_CONSTANT = 6.67/(10**11)
 #stefan-boltzmann constant
 SB_CONSTANT = 5.67/(10**8)
+#Thomson scattering
+ST_CONSTANT = 6.65/(10**29)
+#speed of light
+C_CONSTANT = 3*(10**8)
+
+def ISCO(mbh):
+	return (6*G_CONSTANT*mbh)/(C_CONSTANT**2)
 
 def tempOfRadius(mbh,mdot,r):
