828fe48436694d28f10d93f695e14ac1a48f34b2
bleezmo <joshuarogers10@gmail.com>
2014-06-13 17:03:10 -0400

    forgot to add

---
+++ q0924.py
@@ -0,0 +1,77 @@
+import imp
+import interpolater
+import map_objects
+from quasar_data import *
+
+import gc
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+
+class Header:
+	def __init__(self,file):
+		self.mag_avg = float(file.readline().split(" ")[0])
+		array_size = file.readline()[:-1]
+		self.IMAGE_WIDTH = int(array_size)
+		self.IMAGE_HEIGHT = int(array_size)
+		self.quasar_size = int(file.readline()[:-1])
+		self.kappa_gamma = file.readline()[:-1]
+
+def reloadModules():
+	imp.reload(interpolater)
+	imp.reload(map_objects)
+
+def loadDisk(headerFile, magMapFile,wavelengths,einsteinRadius,diskSize,annulus_removed):
+	print("loading magnification map and header file")
+	file = open(magMapFile,"rb")
+	header = Header(open(headerFile))
+	pixel_size = (header.quasar_size * einsteinRadius)/header.IMAGE_WIDTH
+	mag_array = np.fromfile(file,np.dtype("i4")).reshape(header.IMAGE_WIDTH,header.IMAGE_HEIGHT)
+	file.close()
+	disk = map_objects.Disk(diskSize[0],diskSize[1],pixel_size)
+	print("computing wavelengths in disk")
+	stepsize = diskSize[1]//200
+	disk.computeWavelengths(wavelengths,smooth_step = stepsize if stepsize > 0 else 1,annulus_removed=annulus_removed)
+	print("applying magnification to wavelengths")
+	disk.applyMagnification(mag_array)
+	return disk
+
+#wavelengths are assumed to be in nanometers
+def start(headerFiles,magMapFiles,red_shift,wavelengths,einsteinRadius,diskSize,annulus_removed):
+	reloadModules()
+	shifted_wavelengths = [w/(red_shift+1) for w in wavelengths]
+	if(annulus_removed != None):
+		annulus_removed = (annulus_removed[0]/(red_shift+1),annulus_removed[1])
+	disk1 = loadDisk(headerFiles[0],magMapFiles[0],shifted_wavelengths,einsteinRadius,diskSize,annulus_removed)
+	disk2 = loadDisk(headerFiles[1],magMapFiles[1],shifted_wavelengths,einsteinRadius,diskSize,annulus_removed)
+
+	print("generating plot")
+	# plot([w.wavelength for w in disk1.wavelengths],\
+	# 	[w.total_magnification for w in disk1.wavelengths],\
+	# 	'bo')
+
+	# plot([w.wavelength for w in disk2.wavelengths],\
+	# 	[w.total_magnification for w in disk2.wavelengths],\
+	# 	'ro')
+
+	mag_ratios = []
+	for i in range(len(disk1.wavelengths)):
+		mag_ratios.append(disk1.wavelengths[i].total_magnification/disk2.wavelengths[i].total_magnification)
+
+	plt.plot(wavelengths,mag_ratios,'go')
+	#adding some text. the offsets are just for positioning
+	for i in range(len(wavelengths)):
+		plt.text(wavelengths[i],mag_ratios[i]," ({:.3f},{:.3f})".format(wavelengths[i],mag_ratios[i]),fontsize=10)
+
+	plt.show()
+	disk1 = None
+	disk2 = None
+	mag_ratios = None
+	gc.collect()
+
+headers = ("0924/Image_A/s90/mapmeta.dat","0924/Image_D/s90/mapmeta.dat")
+magFiles = ("0924/Image_A/s90/map.bin","0924/Image_D/s90/map.bin")
+diskSize = ([5000,5000],200)
+annulus_removed = None
+start(headers,magFiles,q3[0],q3[1],q3[2],diskSize,annulus_removed)
---
+++ quasar_data.py
@@ -0,0 +1,64 @@
+# MGJ0414+0534 (rs=2.64)wavelengths(nm):
+# WFC3/UVIS Imaging F467M - 468.3
+# WFC3/UVIS Imaging F621M - 621.9
+# WFC3/UVIS Imaging F763M - 761.2
+# WFC3/UVIS Imaging F845M - 843.6
+# WFC3/IR Imaging F125W - 1248.6
+q1=(2.64,(468.3,621.9,761.2,843.6,1248.6))
+
+#RXJ0911+0551 (rs=2.80)wavelengths(nm):
+# WFC3/UVIS Imaging F547M - 544.7
+# WFC3/UVIS Imaging F621M - 621.9
+# WFC3/UVIS Imaging F689M - 687.6
+# WFC3/UVIS Imaging F845M - 843.6
+# WFC3/IR Imaging F125W - 1248.6
+# WFC3/IR Imaging F160W - 1536.9
+q2=(2.8,(544.7,621.9,687.6,843.6,1248.6,1536.9))
+
+#SDSSJ0924+0219 (rs=1.524) wavelengths(nm):
+# WFC3/UVIS Imaging F410M - 410.9
+# WFC3/UVIS Imaging F547M - 544.7
+# WFC3/UVIS Imaging F621M - 621.9
+# WFC3/UVIS Imaging F845M - 843.6
+# WFC3/IR Imaging F105W - 1055.2
+# WFC3/IR Imaging F160W - 1536.9
+# Einstein radius - 4.372277 x 10^14 m
+q3=(1.524,(410.9,544.7,621.9,843.6,1055.2,1536.9), 437227700000000)
+
+# PG1115+080 (rs=1.72) wavelengths(nm):
+# WFC3/UVIS Imaging F410M - 410.9
+# WFC3/UVIS Imaging F621M - 621.9
+# WFC3/UVIS Imaging F845M - 843.6
+# WFC3/IR Imaging F125W - 1248.6
+# WFC3/IR Imaging F160W - 1536.9
+# WFC3/IR Imaging F105W - 1055.2
+q4=(1.72,(410.9,621.9,843.6,1248.6,1536.9,1055.2))
+
+# B1422+231 (rs=3.62) wavelengths(nm):
+# WFC3/UVIS Imaging F621M - 621.9
+# WFC3/UVIS Imaging F763M - 761.2
+# WFC3/UVIS Imaging F845M - 843.6
+# WFC3/IR Imaging F125W - 1248.6
+# WFC3/IR Imaging F160W - 1536.9
+# WFC3/IR Imaging F105W - 1055.2
+q5=(3.62,(621.9,761.2,843.6,1248.6,1536.9,1055.2))
+
+# WFIJ2026-4536 (rs=2.23)wavelength(nm):
+# WFC3/UVIS Imaging F410M - 410.9
+# WFC3/UVIS Imaging F547M - 544.7
+# WFC3/UVIS Imaging F689M - 687.6
+# WFC3/UVIS Imaging F763M - 761.2
+# WFC3/UVIS Imaging F845M - 843.6
+# WFC3/IR Imaging F105W - 1055.2
+# WFC3/IR Imaging F125W - 1248.6
+# WFC3/IR Imaging F160W - 1536.9
+q6=(2.23,(410.9,544.7,687.6,761.2,843.6,1055.2,1248.6,1536.9))
+
+# WFIJ2033-4723 (rs=1.66) wavelengths(nm):
+# WFC3/UVIS Imaging F467M - 468.3
+# WFC3/UVIS Imaging F621M - 621.9
+# WFC3/UVIS Imaging F763M - 761.2
+# WFC3/UVIS Imaging F814W - 802.4
+# WFC3/IR Imaging F125W - 1248.6
+# WFC3/IR Imaging F160W - 1536.9
+q7=(1.66,(468.3,621.9,761.2,802.4,1248.6,1536.9))
